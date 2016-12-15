cd09452f09e5eddffab6bc30983f7c24421fcdb4
Gabriel Gimenez <12-11006@usb.ve>
2016-12-15 17:26:28 -0400

    form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProfilePage } from '../profile/profile';
 import { ContactsPage } from '../contacts/contacts';
+import { FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
 })
 export class HomePage {
-  constructor(public nav: NavController) {
+  public transferForm;
+  constructor(public nav: NavController, public formBuilder: FormBuilder) {
     this.nav = nav;
+    this.transferForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required])],
+      money: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
   }
+
 
   goToProfile() {
     this.nav.push(ProfilePage);
